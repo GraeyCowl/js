027faf574669ffb0765afd183bd3efa339c9168a
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-05 12:44:27 -0700

    Storing constants

--- proto-java/javascript.js
+++ proto-java/javascript.js
@@ -1,3 +1,8 @@
-const myBirthday = '18.04.1982';
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
 
-myBirthday = '01.01.2001'; // error, can't reassign the constant!
+// ...when we need to pick a color
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
