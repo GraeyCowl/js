fbe037c54ecb15eec41ad599d6038e900bc2e48b
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-05 12:43:44 -0700

    Double consant

--- proto-java/javascript.js
+++ proto-java/javascript.js
@@ -1,10 +1,3 @@
-let hello = 'Hello world!';
+const myBirthday = '18.04.1982';
 
-let message;
-
-// copy 'Hello world' from hello into message
-message = hello;
-
-// now two variables hold the same data
-alert(hello); // Hello world!
-alert(message); // Hello world!
+myBirthday = '01.01.2001'; // error, can't reassign the constant!
