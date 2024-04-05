f3084f5d68f49bd0c13b90706f8c38f12eee0729
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-05 13:20:07 -0700

    Done tasks

--- proto-java/var task/javascript.js
+++ proto-java/var task/javascript.js
@@ -1,9 +1,18 @@
 let admin, name; 
 
 name = "John";
-
 admin = name;
 
 let ourPlanetName = "Earth";
+let currentUserName = "John";
 
-alert( admin);
+const BIRTHDAY = '18.04.1982';
+// const age = someCode(BIRTHDAY);
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "#00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_BLUE;
+alert(color);
