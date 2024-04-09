aa95b068a91741d5e646745358af1cc35cd923f7
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-08 22:13:48 -0700

    Add thicker outline

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -10,6 +10,8 @@ window.addEventListener('resize', function(){
 
 ctx.fillStyle = 'red';
 ctx.strokeStyle = 'red';
+ctx.lineWidth = 5;
 ctx.beginPath()
 ctx.arc(100, 100, 50, 0, Math.PI * 2);
 ctx.stroke();
+console.log(ctx)
