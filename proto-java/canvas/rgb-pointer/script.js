e23d7a565ff9a339dbc51c4aecf0180c72c4a37b
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-08 22:35:58 -0700

    Redraw circle on itself

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -1,18 +1,30 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 window.addEventListener('resize', function(){
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
 });
 
-ctx.fillStyle = 'blue';
+const mouse = {
+  x : null,
+  y : null,
+}
+canvas.addEventListener('click', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+
+function drawCircle(){
+  ctx.fillStyle = 'blue';
 ctx.strokeStyle = 'red';
 ctx.lineWidth = 25;
-ctx.beginPath()
+ctx.beginPath();
 ctx.arc(100, 100, 50, 0, Math.PI * 2);
 ctx.stroke();
 ctx.fill();
-console.log(ctx);
+}
+drawCircle();
