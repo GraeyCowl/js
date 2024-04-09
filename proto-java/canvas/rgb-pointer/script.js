5d9dfbc0d8d737d68d2a24cd79793c1c0df3b8ac
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-08 22:13:37 -0700

    Red outline

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -9,6 +9,7 @@ window.addEventListener('resize', function(){
 });
 
 ctx.fillStyle = 'red';
+ctx.strokeStyle = 'red';
 ctx.beginPath()
 ctx.arc(100, 100, 50, 0, Math.PI * 2);
-ctx.fill();
+ctx.stroke();
