8e6c7673ff9759853b7473eecd87636c934ee94d
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-08 22:07:33 -0700

    Window resize wipe fixed

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -6,6 +6,8 @@ canvas.height = window.innerHeight
 window.addEventListener('resize', function(){
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, 50, 50);
 });
 
 ctx.fillStyle = 'white';
