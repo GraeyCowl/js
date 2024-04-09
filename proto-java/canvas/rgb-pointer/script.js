977a517ebffffa7ebd0cd238282bf3a71776d468
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-08 22:06:05 -0700

    Resize partial fix

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -1,6 +1,12 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+window.addEventListener('resize', function(){
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight
+});
 
 ctx.fillStyle = 'white';
 ctx.fillRect(10, 10, 50, 50);
