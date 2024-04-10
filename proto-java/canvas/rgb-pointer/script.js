47d40e511fdb9abd1200443bb0e2927127ac3ad2
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-10 14:58:45 -0700

    Spawn shrink despawn

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -30,7 +30,7 @@ class Particle {
     //this.y = mouse.y;
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
@@ -38,12 +38,13 @@ class Particle {
   update(){
     this.x += this.speedX;
     this.y += this.speedY;
+    if(this.size > 0.2) this.size -= 0.1;
   }
 
   draw(){
     ctx.fillStyle = 'blue';
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
   }
 }
@@ -55,15 +56,18 @@ function init(){
 }
 
 init();
-console.log(particlesArray);
 
 function handleParticles(){
   for (let i = 0; i < particlesArray.length; i++){
     particlesArray[i].update();
     particlesArray[i].draw();
+    if (particlesArray[i].size <= 0.3){
+      particlesArray.splice(i, 1);
+      console.log(particlesArray.length);
+      i--;
+    }
   }
 }
-
 function animate(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   handleParticles();
