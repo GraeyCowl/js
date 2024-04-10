75f65b99d221d76eedc42ee7c3230355886d109b
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-10 16:59:45 -0700

    Syntax and spacing

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -45,21 +45,19 @@ class Particle {
     this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
-    this.color = 'hsl(' + hue + ', 100%, 50%)'
+    this.color = 'hsl(' + hue + ', 100%, 50%)';
   }
-
   update(){
     this.x += this.speedX;
     this.y += this.speedY;
-    if(this.size > 0.2) this.size -= 0.1;
+    if (this.size > 0.2) this.size -= 0.1;
   }
-
   draw(){
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
-  }
+   }
 }
 
 function handleParticles(){
