52cf09deb4e11cc47313d3dbde02f61f45465510
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-10 14:03:48 -0700

    AddParticle and  particlesArray

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener('resize', function(){
   canvas.width = window.innerWidth
@@ -22,28 +23,40 @@ canvas.addEventListener('mousemove', function(event){
   mouse.y = event.y;
 })
 
-function drawCircle(){
-  ctx.fillStyle = 'blue';
-  ctx.strokeStyle = 'red';
-  ctx.lineWidth = 25;
-  ctx.beginPath();
-  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
-  ctx.stroke();
-  ctx.fill();
-}
-
 class Particle {
   constructor(){
-    thix.x = mouse.x;
+    this.x = mouse.x;
     this.y = mouse.y;
     this.size = Math.random() * 5 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
+  update(){
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+  draw(){
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 25;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.stroke();
+    ctx.fill();
+  }
 }
+
+function init(){
+  for (let i = 0; i < 100; i++){
+    particlesArray.push(new Particle());
+  }
+}
+
+init();
+console.log(particlesArray);
+
 function animate(){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
   requestAnimationFrame(animate);
 }
 
