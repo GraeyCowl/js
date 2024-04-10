a782f7bb9e978990da6d599d8fd5e38e7d69016a
GraeyCowl <GraeyCowlCourier@proton.me>
2024-04-10 16:05:32 -0700

    Start pythagore constellation

--- proto-java/canvas/rgb-pointer/script.js
+++ proto-java/canvas/rgb-pointer/script.js
@@ -61,17 +61,23 @@ function handleParticles(){
   for (let i = 0; i < particlesArray.length; i++){
     particlesArray[i].update();
     particlesArray[i].draw();
+
+    for (let j = i; j <particlesArray.length; j++){
+      const dx = particlesArray[i].x - particlesArray[j].x;
+      const dy = particlesArray[i].y - particlesArray[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+    }
     if (particlesArray[i].size <= 0.3){
       particlesArray.splice(i, 1);
-      // console.log(particlesArray.length);
+      console.log(particlesArray.length);
       i--;
     }
   }
 }
 function animate(){
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = 'rgba(0, 0, 0, 0.02)';
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   hue+=2;
   requestAnimationFrame(animate);
