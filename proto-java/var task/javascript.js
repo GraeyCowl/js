907a8af9a78b98e06f8666846388ed15656a1eff
GraeyCowl <GraeyCowlCourier@proton.me>
2024-06-12 16:20:58 -0700

    Add operator

--- proto-java/var task/javascript.js
+++ proto-java/var task/javascript.js
@@ -16,3 +16,19 @@ const COLOR_ORANGE = "#FF7F00";
 
 let color = COLOR_BLUE;
 // alert(color); End of Var, Start Oper
+let a = 1;
+a = -a;
+console.log ( a ); // -1, unary negation was applied
+
+let b = 1, c = 3;
+console.log( c - b ); // 2, binary minus subtracts values
+
+console.log( 5 % 2 ); // 1, the remainder of 5 divided by 2
+console.log( 8 % 3 ); // 2, the remainder of 8 divided by 3
+console.log( 8 % 4 ); // 0, the remainder of 8 divided by 4
+
+console.log( 2 ** 2 ); // 2² = 4
+console.log( 2 ** 3 ); // 2³ = 8
+console.log( 2 ** 4 ); // 2⁴ = 16
+console.log( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+console.log( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
