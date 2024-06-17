4edd1119b170655280cb258ff7b695d9320f196f
YourGraeyCowl <GraeyCowlCourier@proton.me>
2024-06-17 13:54:45 -0700

    Change to .wav

--- demo/AudioVis/script.js
+++ demo/AudioVis/script.js
@@ -2,5 +2,6 @@ const button1 = document.getElementById('button1')
 let = audio1 = new Audio();
 audio1.src = 'spell.wav';
 
-button1.addEventListener('click', function(){audio1.play();
+button1.addEventListener('click', function(){
+    audio1.play();
 });
